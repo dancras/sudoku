0f6c714670c127f8cbc8e9aa9a947480ed99ce01
Daniel Howlett <dan@dancras.co.uk>
2022-02-08 17:12:21 +0000

    Move to vitest and fix testing-library preact/user-event interop

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,17 @@
 /* eslint @typescript-eslint/indent: ["error", 2] */
+/// <reference types="vitest" />
 import { defineConfig } from 'vite';
-import preact from '@preact/preset-vite'
+import preact from '@preact/preset-vite';
 import path from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [preact()],
+  test: {
+    globals: true,
+    environment: 'jsdom',
+    setupFiles: ['./setupTests.ts']
+  },
   server: {
     host: '0.0.0.0'
   },
