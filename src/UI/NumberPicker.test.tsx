a3faa031ddfc3315e5a0c40727e71c795c6f89b4
Daniel Howlett <dan@dancras.co.uk>
2022-02-08 14:43:56 +0000

    Setup jest and module paths

---
+++ src/UI/NumberPicker.test.tsx
@@ -0,0 +1,5 @@
+import NumberPicker from 'src/UI/NumberPicker';
+
+test('numbers 1 to 9 are displayed', () => {
+    expect(NumberPicker()).toEqual(true);
+});
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,20 @@
-import { defineConfig } from 'vite'
+/* eslint @typescript-eslint/indent: ["error", 2] */
+import { defineConfig } from 'vite';
 import preact from '@preact/preset-vite'
+import path from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [preact()]
-})
+  plugins: [preact()],
+  server: {
+    host: '0.0.0.0'
+  },
+  preview: {
+    host: '0.0.0.0',
+  },
+  resolve: {
+    alias: {
+      'src': path.resolve(__dirname, './src')
+    }
+  }
+});
