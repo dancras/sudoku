5f67b1eb898e05327ca00b206004691e4c218425
Daniel Howlett <dan@dancras.co.uk>
2022-03-01 15:00:14 +0000

    Final tweaks to the onboarding messaging

--- src/Onboarding/index.tsx
+++ src/Onboarding/index.tsx
@@ -8,6 +8,10 @@ export type OnboardingStorageSchema = {
     seen: Record<string, boolean>
 }
 
+function isTouchScreen() {
+    return window.matchMedia('(hover: none)').matches;
+}
+
 export function setupOnboarding(
     storage: Persistence<OnboardingStorageSchema>,
     messages$: Subject<MessageData>,
@@ -39,10 +43,14 @@ export function setupOnboarding(
 
     let solvingUpdates = 0;
 
+    const tapOrClick = <i>{ isTouchScreen() ? 'Tap' : 'Click' }</i>;
+    const doubleTapOrClick = <i>{ isTouchScreen() ? 'Double Tap' : 'Double Click' }</i>;
+    const tappingOrClicking = <i>{ isTouchScreen() ? 'Tapping' : 'Clicking' }</i>;
+
     addMessage('welcome', <>
         <p>Welcome to my Sudoku app.</p>
         <p>I&apos;ll be sharing a few details the first time you encounter things.</p>
-        <p><i>Tap or Click</i> anywhere to dismiss a message.</p>
+        <p>{ tapOrClick } anywhere to dismiss a message.</p>
     </>);
 
     addMessage('about', <>
@@ -61,18 +69,18 @@ export function setupOnboarding(
         if (status === SudokuGameStatus.Creating) {
 
             addMessage('create_start', <>
-                <p>Right now you&apos;re in Create Mode.</p>
+                <p>Right now you&apos;re in <b>Create Mode</b>.</p>
                 <p>Here you can add a puzzle to play and share.</p>
             </>);
 
-            addMessage('photo_upload', <>
-                <p>The easiest way is by photo upload.</p>
-                <p>You can take a photo or upload an image of a Sudoku Grid.</p>
+            addMessage('photo_import', <>
+                <p>The easiest way is by <b>Photo Import</b>.</p>
+                <p>You can take a photo or choose an image of a Sudoku Grid.</p>
                 <p>The entire grid must be in the photo or stange things might happen..</p>
             </>);
 
-            addMessage('photo_upload_button',
-                <p>Tap or Click the camera icon to use it.</p>
+            addMessage('photo_import_button',
+                <p>{ tapOrClick } the camera icon to use it.</p>
                 , {
                     target: 'ButtonBar',
                     button: 2
@@ -80,18 +88,18 @@ export function setupOnboarding(
             );
 
             addMessage('create_manual',
-                <p>Otherwise you can edit the grid directly.</p>
+                <p>Otherwise you can edit the grid manually.</p>
             );
 
             addMessage('create_choose_number',
-                <p>Start by choosing a number.</p>
+                <p>Start by { tappingOrClicking } a number to select it.</p>
                 , {
                     target: 'NumberPicker'
                 }
             );
 
             addMessage('create_toggle_cell',
-                <p>Then Tap or Click a cell to toggle it on or off.</p>
+                <p>Then { tapOrClick } a cell to add or remove that number.</p>
                 , {
                     target: 'SudokuGrid'
                 }
@@ -99,18 +107,18 @@ export function setupOnboarding(
         }
 
         if (appUpdate && appUpdate.type === 'LoadGameUpdate' && appUpdate.startGame === false) {
-            addMessage('photo_upload_complete', <>
+            addMessage('photo_import_complete', <>
                 <p>Your Phone or Computer has just trained a simple AI to understand the photo in real time.</p>
                 <p>First check that it&apos;s done a good job.</p>
             </>);
 
-            addMessage('photo_upload_advice', <>
+            addMessage('photo_import_advice', <>
                 <p>For best results keep the grid straight and centered.</p>
-                <p>Try taking it from further away if it&apos;s on a screen.</p>
+                <p>If it&apos;s from a screen, try zooming in and taking the photo from further away.</p>
             </>);
 
-            addMessage('photo_upload_fix',
-                <p>You can always fix any missing or incorrect parts manually before Starting.</p>
+            addMessage('photo_import_fix',
+                <p>You can always fix any missing or incorrect parts manually before starting.</p>
             );
         }
 
@@ -119,7 +127,7 @@ export function setupOnboarding(
             gameUpdate !== undefined && status === SudokuGameStatus.Creating
         ) {
             addMessage('start_game_button',
-                <p>This is the Start Game button for when you are done creating.</p>
+                <p>This is the <b>Start Game Button</b> for when you are done creating.</p>
                 , {
                     target: 'ButtonBar',
                     button: 1
@@ -129,8 +137,8 @@ export function setupOnboarding(
 
         if (gameUpdate !== undefined) {
             addMessage('undo_redo', <>
-                <p>The Undo and Redo buttons are available to correct errors.</p>
-                <p>They even work if you Start or Clear the game</p>
+                <p>The <b>Undo and Redo Buttons</b> are available to correct errors.</p>
+                <p>They even work if you start or clear the game.</p>
             </>, {
                 target: 'ButtonBar',
                 button: 0,
@@ -140,27 +148,32 @@ export function setupOnboarding(
 
         if (appUpdate && appUpdate.type === 'LoadGameUpdate' && appUpdate.startGame === true) {
             addMessage('arrive_from_link',
-                <p>You&apos;ve arrived from a shared Puzzle link so you can start solving right away.</p>
+                <p>You&apos;ve arrived from a shared puzzle link so you can start solving right away.</p>
             );
         }
 
         if (appUpdate && appUpdate.type === 'StartGameUpdate') {
             addMessage('start_game',
-                <p>The puzzle cells are now locked and you&apos;re in Solve Mode.</p>
+                <p>The puzzle cells are now locked and you&apos;re in <b>Solve Mode</b>.</p>
             );
         }
 
         if (status === SudokuGameStatus.Solving) {
             addMessage('solving_choose_number',
-                <p>Start by choosing a number.</p>
+                <p>Start by { tappingOrClicking } a number to select it.</p>
                 , {
                     target: 'NumberPicker'
                 }
             );
 
-            addMessage('solving_toggle_cell', <>
-                <p>Tap or Click a Cell to add or remove the chosen number as a Candidate.</p>
-                <p>Double Tap or Click to solve the cell with the chosen number.</p>
+            addMessage('solving_toggle_cell_candidate', <>
+                <p>{ tapOrClick } a cell to add or remove the chosen number as a Candidate.</p>
+            </>, {
+                target: 'SudokuGrid'
+            });
+
+            addMessage('solving_toggle_cell_solution', <>
+                <p>{ doubleTapOrClick } to add or remove the chosen number as a Solution.</p>
             </>, {
                 target: 'SudokuGrid'
             });
@@ -168,8 +181,8 @@ export function setupOnboarding(
 
         if (gameUpdate !== undefined && gameUpdate.type === 'CellUpdate' && status === SudokuGameStatus.Solving) {
             addMessage('reset_button', <>
-                <p>The Reset button will start the puzzle again.</p>
-                <p>Tap or Click it twice to go back to Create Mode.</p>
+                <p>The <b>Reset Button</b> will start the puzzle again.</p>
+                <p>{ tapOrClick } it twice to go back to Create Mode.</p>
                 <p>These actions can be undone.</p>
             </>, {
                 target: 'ButtonBar',
@@ -186,7 +199,7 @@ export function setupOnboarding(
 
         if (solvingUpdates === 20) {
             addMessage('share_button', <>
-                <p>Tap or Click the Share button any time to share puzzles with friends.</p>
+                <p>{ tapOrClick } the <b>Share Button</b> any time to share puzzles with friends.</p>
                 <p>Shared puzzles won&apos;t include any of your current progress.</p>
                 <p>I&apos;ll leave you in peace now. Enjoy!</p>
             </>, {
@@ -198,14 +211,14 @@ export function setupOnboarding(
         if (status === SudokuGameStatus.Solved) {
             addMessage('complete', <>
                 <p>All done. I hope this first puzzle was a smooth experience.</p>
-                <p>This button will clear the grid and return to Create Mode.</p>
+                <p>The <b>Bin Button</b> will clear the grid and return to Create Mode.</p>
             </>, {
                 target: 'ButtonBar',
                 button: 1
             });
 
             addMessage('final_share',
-                <p>Maybe you want to share the puzzle before you do that?</p>
+                <p>Maybe you want to <b>Share</b> the puzzle before you do that?</p>
                 , {
                     target: 'ButtonBar',
                     button: 2
